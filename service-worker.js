7f96b77c13c4de7f0b9167db05b2f2a1908064ae
Stefan-van-Dijk <stefanvandijk@me.com>
2026-09-16 21:31:43 +0200

    Fix cache cleanup in 0.31.2

--- service-worker.js
+++ service-worker.js
@@ -24,7 +24,7 @@ self.addEventListener('install',event=>{
 self.addEventListener('activate',event=>{
   event.waitUntil(
     caches.keys()
-      .then(keys=>Promise.all(keys.filter(key=>key.startsWith('kmreg-shell-')&&key!==CACHE).map(key=>caches.delete(k))))
+      .then(keys=>Promise.all(keys.filter(key=>key.startsWith('kmreg-shell-')&&key!==CACHE).map(key=>caches.delete(key))))
       .then(()=>self.clients.claim())
       .then(()=>self.clients.matchAll({type:'window'}))
       .then(clients=>Promise.all(clients.map(client=>client.navigate(client.url).catch(()=>null))))
