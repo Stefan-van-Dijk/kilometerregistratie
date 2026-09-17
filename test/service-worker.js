1aa1717f8f0bb190410b71b56e53633c4c7463e7
Stefan-van-Dijk <stefanvandijk@me.com>
2026-09-17 18:06:48 +0200

    Cache Log v2 JSON builder in test PWA

--- test/service-worker.js
+++ test/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='kmreg-test-shell-0.31.10-test.1';
-const SHELL=['./','./index.html','./shell-ui.js?v=0.31.10-test.1','./shell-ui-stable.js?v=0.31.10-test.1','./id-converter.html','./manifest.webmanifest','./app-icon.svg'];
+const CACHE='kmreg-test-shell-0.31.10-json-v2';
+const SHELL=['./','./index.html','./log-json-v2.js?v=2.0.0','./shell-ui.js?v=0.31.10-test.1','./shell-ui-stable.js?v=0.31.10-test.1','./id-converter.html','./manifest.webmanifest','./app-icon.svg'];
 
 function injectShellScript(response){
   if(!response)return response;
