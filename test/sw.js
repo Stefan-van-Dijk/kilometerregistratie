52d0673cc12ede8292d0179f63c2c5b576b6e818
Stefan-van-Dijk <stefanvandijk@me.com>
2026-09-17 17:52:22 +0200

    Add isolated Log test environment

---
+++ test/sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'kmreg-test-v3';
+const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icons/icon.svg'];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(Promise.all([
+    caches.keys().then(keys => Promise.all(keys.filter(k => k.startsWith('kmreg-test-') && k !== CACHE).map(k => caches.delete(k)))),
+    self.clients.claim()
+  ]));
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  // Voor navigatie en appbestanden eerst het netwerk proberen, zodat GitHub-updates snel zichtbaar zijn.
+  event.respondWith(
+    fetch(event.request).then(resp => {
+      const clone = resp.clone();
+      caches.open(CACHE).then(c => c.put(event.request, clone));
+      return resp;
+    }).catch(() => caches.match(event.request).then(hit => hit || caches.match('./index.html')))
+  );
+});
